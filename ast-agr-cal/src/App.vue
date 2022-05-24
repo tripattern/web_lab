826030700314d20f0b36cfa99b8a328523456087
tripattern <systems@tripattern.com>
2022-05-24 21:30:14 +0200

    aac: added moon logo and moved notes to README.md

--- ast-agr-cal/src/App.vue
+++ ast-agr-cal/src/App.vue
@@ -5,7 +5,8 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <!--<img alt="Moon logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
+    <img alt="Moon logo" class="logo" src="https://openclipart.org/image/400px/17926" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
