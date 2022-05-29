db9fe7c9c4c2569b7dea7cc53e45df1833c7c93b
tripattern <systems@tripattern.com>
2022-05-29 22:16:52 +0200

    boilerplate again with jsx and cypress

--- ast-agr-cal/src/main.ts
+++ ast-agr-cal/src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import router from './router'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
 
-loadFonts()
+const app = createApp(App)
 
-createApp(App)
-  .use(router)
-  .use(vuetify)
-  .mount('#app')
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
