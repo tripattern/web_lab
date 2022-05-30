9301ee1ea33d45e9ada057ee774d50a5589fe9fa
tripattern <systems@tripattern.com>
2022-05-30 14:16:45 +0200

    aac: had to drop down to version 2 of vue

--- ast-agr-cal/src/main.ts
+++ ast-agr-cal/src/main.ts
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue'
+import VueCompositionAPI, { createApp, h } from '@vue/composition-api'
+import { createPinia, PiniaVuePlugin } from 'pinia'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router'
+
+Vue.use(VueCompositionAPI)
+
+const app = createApp({
+  router,
+  pinia: createPinia(),
+  render: () => h(App)
+})
+app.use(PiniaVuePlugin)
+
+app.mount('#app')
