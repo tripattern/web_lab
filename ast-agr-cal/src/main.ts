9d37eb7d815382391e1458922578b83bd8c7f66b
tripattern <systems@tripattern.com>
2022-05-30 14:30:01 +0200

    aac: back to vue 3

--- ast-agr-cal/src/main.ts
+++ ast-agr-cal/src/main.ts
@@ -1,17 +1,12 @@
-import Vue from 'vue'
-import VueCompositionAPI, { createApp, h } from '@vue/composition-api'
-import { createPinia, PiniaVuePlugin } from 'pinia'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-Vue.use(VueCompositionAPI)
+const app = createApp(App)
 
-const app = createApp({
-  router,
-  pinia: createPinia(),
-  render: () => h(App)
-})
-app.use(PiniaVuePlugin)
+app.use(createPinia())
+app.use(router)
 
 app.mount('#app')
--- ast-agr-cal/vite.config.ts
+++ ast-agr-cal/vite.config.ts
@@ -1,27 +1,12 @@
-import { fileURLToPath } from 'url'
+import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
-import legacy from '@vitejs/plugin-legacy'
-import { createVuePlugin as vue2 } from 'vite-plugin-vue2'
-// @ts-ignore
-import vueTemplateBabelCompiler from 'vue-template-babel-compiler'
-import scriptSetup from 'unplugin-vue2-script-setup/vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue2({
-      jsx: true,
-      vueTemplateOptions: {
-        compiler: vueTemplateBabelCompiler
-      }
-    }),
-    scriptSetup(),
-    legacy({
-      targets: ['ie >= 11'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
-    })
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
