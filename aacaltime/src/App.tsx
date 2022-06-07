859c86b531db9bef84ca33ba73470e18e48c1b8e
tripattern <systems@tripattern.com>
2022-06-07 14:38:38 +0200

    aact: added start of minimalist theme

--- aacaltime/src/App.tsx
+++ aacaltime/src/App.tsx
@@ -2,14 +2,28 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import MoonPhase from './components/MoonPhase/MoonPhase';
+import {createTheme, ThemeProvider} from '@mui/material';
+
+const minTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#FFFFFF',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+    },
+});
 
 function App() {
-  return (
-      <div>
-        <NavBar/>
-        <MoonPhase/>
-      </div>
-  );
+    return (
+        <ThemeProvider theme={minTheme}>
+            <div>
+                <NavBar/>
+                <MoonPhase/>
+            </div>
+        </ThemeProvider>
+    );
 }
 
 export default App;
