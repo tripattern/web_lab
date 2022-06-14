6e03fbc874f416c6d99fb9cbc274a5966746472b
tripattern <systems@tripattern.com>
2022-06-14 13:17:31 +0200

    aact: refactoring DateTimeHelpers.tsx

--- aacaltime/src/utils/DateTimeHelpers/DateTimeHelpers.tsx
+++ aacaltime/src/utils/DateTimeHelpers/DateTimeHelpers.tsx
@@ -3,7 +3,6 @@ export function GetCorrectedGregorianDateAsString(myDate: Date): string {
     const dayDate: number = myDate.getUTCDate();
     const realMonthWithLeadingZero: string = (realMonth < 10) ? '0' + realMonth : String(realMonth);
     const realDayWithLeadingZero: string = (dayDate < 10) ? '0' + dayDate : String(dayDate);
-    console.log(myDate.getUTCFullYear().toString() + "-" + realMonthWithLeadingZero + "-" + realDayWithLeadingZero);
     return myDate.getUTCFullYear().toString() + "-" + realMonthWithLeadingZero + "-" + realDayWithLeadingZero;
 }
 
